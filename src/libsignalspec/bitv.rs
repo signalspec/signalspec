c772bdcede073ea4f3925650284cab272525c6b0
Kevin Mehall <km@kevinmehall.net>
2014-02-22 20:39:32 -0500

    rust: fix bitv

--- src/libsignalspec/bitv.rs
+++ src/libsignalspec/bitv.rs
@@ -981,7 +981,8 @@ impl<'a> Iterator<uint> for BitPositions<'a> {
 
 #[cfg(test)]
 mod tests {
-    use extra::test::BenchHarness;
+    extern crate test;
+    use self::test::BenchHarness;
 
     use bitv::{Bitv, SmallBitv, BigBitv, BitvSet, from_bools, from_fn,
                from_bytes, concat};
