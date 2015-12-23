3a8a036705de005edd8b2d30b488cdd4a96a02ab
Kevin Mehall <km@kevinmehall.net>
2015-12-23 12:37:47 -0700

    Split into library and binary crates

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![allow(dead_code)]
+#![feature(plugin)]
+#![feature(box_syntax)]
+#![feature(box_patterns)]
+#![feature(str_char)]
+#![feature(iter_arith)]
+#![plugin(peg_syntax_ext)]
+
+extern crate typed_arena;
+extern crate bit_set;
+extern crate vec_map;
+extern crate ref_slice;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
+
+use std::fs;
+use std::io::prelude::*;
+
+use session::Process;
+
+#[macro_use] mod session;
+mod data;
+mod resolve;
+mod eval;
+mod ast;
+mod exec;
+mod dumpfile;
+mod vcd;
+mod connection_io;
+mod test_runner;
+
+peg_file! grammar("signalspec.rustpeg");
+
+pub fn run(source_fname: &String, code: &[String]) -> bool {
+    let sess = session::Session::new();
+
+    let mut source = String::new();
+    fs::File::open(source_fname).unwrap().read_to_string(&mut source).unwrap();
+    let modscope = sess.parse_module(&source).unwrap();
+
+    let mut processes = vec![];
+    let mut shape = data::Shape::null();
+
+    for arg in code {
+        let process_ast = grammar::process(&arg)
+            .unwrap_or_else(|e| panic!("Error parsing argument: {}", e));
+        let process = session::resolve_process(&sess, &modscope, &shape, &process_ast);
+        shape = process.shape_up().clone();
+        processes.push(process);
+    }
+
+    let topmost_mode = shape.data_mode();
+
+    if topmost_mode.up {
+        processes.push(box dumpfile::ValueDumpPrint(shape));
+    }
+
+    session::run_process_chain(processes)
+}
+
+pub use test_runner::run as run_test;
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,6 @@
-#![allow(dead_code)]
-#![feature(plugin)]
-#![feature(box_syntax)]
-#![feature(box_patterns)]
-#![feature(str_char)]
-#![feature(iter_arith)]
-#![plugin(peg_syntax_ext)]
-
-extern crate typed_arena;
-extern crate bit_set;
-extern crate vec_map;
-extern crate ref_slice;
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate log;
+extern crate signalspec;
 extern crate env_logger;
-
-use std::{env, process, fs};
-use std::io::prelude::*;
-
-use session::Process;
-
-#[macro_use] mod session;
-mod data;
-mod resolve;
-mod eval;
-mod ast;
-mod exec;
-mod dumpfile;
-mod vcd;
-mod connection_io;
-mod test_runner;
-
-peg_file! grammar("signalspec.rustpeg");
+use std::{env, process};
 
 fn main() {
     env_logger::init().unwrap();
@@ -38,33 +8,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args[1] == "--test" {
-        return test_runner::run(&args[2]);
+        return signalspec::run_test(&args[2]);
     }
 
-    let sess = session::Session::new();
-
-    let mut source = String::new();
-    fs::File::open(&args[1]).unwrap().read_to_string(&mut source).unwrap();
-    let modscope = sess.parse_module(&source).unwrap();
-
-    let mut processes = vec![];
-    let mut shape = data::Shape::null();
-
-    for arg in &args[2..] {
-        let process_ast = grammar::process(&arg)
-            .unwrap_or_else(|e| panic!("Error parsing argument: {}", e));
-        let process = session::resolve_process(&sess, &modscope, &shape, &process_ast);
-        shape = process.shape_up().clone();
-        processes.push(process);
-    }
-
-    let topmost_mode = shape.data_mode();
-
-    if topmost_mode.up {
-        processes.push(box dumpfile::ValueDumpPrint(shape));
-    }
-
-    let success = session::run_process_chain(processes);
+    let success = signalspec::run(&args[1], &args[2..]);
 
     process::exit(if success { 0 } else { 1 });
 }
