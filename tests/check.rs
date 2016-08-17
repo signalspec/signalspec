8e270352275e6d11c6e50280a4b361b9d93aff4b
Kevin Mehall <km@kevinmehall.net>
2016-08-16 20:28:52 -0700

    Add debug output to run-check tests

--- tests/check.rs
+++ tests/check.rs
@@ -1,7 +1,9 @@
 extern crate signalspec;
+extern crate env_logger;
 use std::process;
 
 fn main() {
+    env_logger::init().unwrap();
     let mut success = true;
     success &= signalspec::run_test("tests/check");
     success &= signalspec::run_test("lib");
