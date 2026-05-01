a923ab2bb64f4a3a2bc07cef6266439d8fca67a4
Kevin Mehall <km@kevinmehall.net>
2026-04-30 22:40:21 -0600

    Use new peg span injection

--- src/syntax/grammar.rs
+++ src/syntax/grammar.rs
@@ -1,29 +1,30 @@
 use super::{ ast, BinOp, FilePos, FileSpan, Spanned, Number };
 use std::char;
 
-const FAKE_SPAN: FileSpan = FileSpan { start: FilePos(0), end: FilePos(0) };
-
 peg::parser!(pub grammar signalspec() for str {
   use peg::ParseLiteral;
 
+  inject span(_input, lpos, rpos) -> FileSpan {
+    FileSpan { start: FilePos(lpos as u32), end: FilePos(rpos as u32) }
+  }
+
   rule spanned<T>(inner: rule<T>) -> Spanned<T>
-    = start:position!() node:inner() end:position!()
-    { Spanned { node, span: FileSpan::new(start, end) } }
+    = node:inner() { Spanned { node, span } }
 
   rule tok_node(s: &str) -> ast::Token
     = span: TOK(s) { ast::Token { span } }
 
   rule tok_node_or_err(s: &'static str) -> Result<ast::Token, ast::Error>
     = n:tok_node(s) { Ok(n) }
-    / p:pos() { Err(ast::Error { span: FileSpan{ start: p, end: p }, expected: s }) }
+    / { Err(ast::Error { span, expected: s }) }
 
   rule recover(expected: &'static str, skip_until: rule<()>) -> ast::Error
-    = start:pos() (!("//" / skip_until()) [_])* end:pos()
-    { ast::Error { span: FileSpan{ start, end }, expected } }
+    = (!("//" / skip_until()) [_])*
+    { ast::Error { span, expected } }
 
   rule skip_recover(expected: &'static str, skip_until: rule<()>) -> ast::Error
-    = start:pos() [^'}' | ')' | ']' | ';'] (!("//" / skip_until()) [_])* end:pos()
-    { ast::Error { span: FileSpan{ start, end }, expected } }
+    = [^'}' | ')' | ']' | ';'] (!("//" / skip_until()) [_])*
+    { ast::Error { span, expected } }
 
   rule module_recovery_set() -> ()
     = RAW_KW("let")
@@ -40,19 +41,17 @@ peg::parser!(pub grammar signalspec() for str {
     / ";"
     / module_recovery_set()
 
-  rule pos() -> FilePos = p:position!() { p.into() }
-
   pub rule module() -> ast::Module
-      = start:position!() _ entries:module_entry()**(_ &[_]) _ end:position!()
-      { ast::Module { span: FileSpan::new(start, end), entries } }
+      = _ entries:module_entry()**(_ &[_]) _
+      { ast::Module { span, entries } }
 
   rule letstmt() -> ast::LetDef
-      = start:pos() tok_let:KW("let") _ name:IDENTIFIER() _ "=" _ expr:expr() end:pos()
-      { ast::LetDef { span: FileSpan{ start, end }, tok_let, name, expr } }
+      = tok_let:KW("let") _ name:IDENTIFIER() _ "=" _ expr:expr()
+      { ast::LetDef { span, tok_let, name, expr } }
 
   rule attribute() -> ast::Attribute
-    = start:pos() tok_at:TOK("@") name:IDENTIFIER() _ args:expr_tup() __ end:pos()
-    { ast::Attribute { span: FileSpan{ start, end }, tok_at, name, args } }
+    = tok_at:TOK("@") name:IDENTIFIER() _ args:expr_tup() __
+    { ast::Attribute { span, tok_at, name, args } }
 
   rule module_entry() -> ast::ModuleEntry
     = l:letstmt() { ast::ModuleEntry::Let(l) }
@@ -61,98 +60,94 @@ peg::parser!(pub grammar signalspec() for str {
     / e:skip_recover("module item", <module_recovery_set()>) {e.into() }
 
     rule def() -> ast::Def
-      = start:pos()
-        attributes:attribute()*
+      = attributes:attribute()*
         tok_with:KW("with") _ bottom:protocol_ref() _
         tok_def:KW("def") _ name:IDENTIFIER() _  params:expr_tup() _ "=" _ process:process()
-        end:pos()
-        { ast::Def { span:FileSpan{start, end}, attributes, bottom, name, params, process } }
+
+        { ast::Def { span, attributes, bottom, name, params, process } }
 
     rule protocol() -> ast::Protocol
-      = start:pos()
-        attributes:attribute()*
+      = attributes:attribute()*
         tok_protocol:KW("protocol") _ name:IDENTIFIER() _ param:expr_tup() _ dir:expr() _
         open:tok_node("{") _ entries:protocol_entry() ** (_ "," _) _ ","? _ close:tok_node_or_err("}")
-        end:pos()
-        { ast::Protocol { span:FileSpan{start, end}, attributes, name, param: param.into(), dir, open, entries, close }}
+
+        { ast::Protocol { span, attributes, name, param: param.into(), dir, open, entries, close }}
 
       rule protocol_entry() -> ast::ProtocolEntry
-        = start:pos() name:IDENTIFIER() _ "(" _ params:COMMASEP(<message_param()>) _ ")" child:(_ ":" _ p:protocol_ref() {p})? end:pos()
-          {ast::ProtocolEntry::Message( ast::ProtocolMessageDef { span: FileSpan{start, end}, name, params, child }) }
-        / start:pos() name:IDENTIFIER() _ ":" _ child_protocol:protocol_ref() end:pos()
-          { ast::ProtocolEntry::Child(ast::ProtocolChild { span: FileSpan { start, end }, name, child_protocol }) }
+        = name:IDENTIFIER() _ "(" _ params:COMMASEP(<message_param()>) _ ")" child:(_ ":" _ p:protocol_ref() {p})?
+          {ast::ProtocolEntry::Message( ast::ProtocolMessageDef { span, name, params, child }) }
+        / name:IDENTIFIER() _ ":" _ child_protocol:protocol_ref()
+          { ast::ProtocolEntry::Child(ast::ProtocolChild { span, name, child_protocol }) }
 
       rule message_param() -> ast::MessageParam
-        = start:pos() name:(name: IDENTIFIER() _ "=" _ {name})? "var" _ "(" _ direction:expr() _ ")" _ expr:expr() end:pos()
-          { ast::MessageParam { span: FileSpan{start, end}, name, direction, expr} }
+        = name:(name: IDENTIFIER() _ "=" _ {name})? "var" _ "(" _ direction:expr() _ ")" _ expr:expr()
+          { ast::MessageParam { span, name, direction, expr} }
 
   rule protocol_ref() -> ast::ProtocolRef
-    = start:pos() name:IDENTIFIER() _ param:expr_tup() end:pos() { ast::ProtocolRef { span: FileSpan{ start, end }, name, param: param.into() } }
+    = name:IDENTIFIER() _ param:expr_tup() { ast::ProtocolRef { span, name, param: param.into() } }
 
   rule block() -> ast::Block
-      = start:pos() open:tok_node("{") _
+      = open:tok_node("{") _
         lets:letstmt()**_ _
         actions:action()**(_ (";" _)?) _
-        close:tok_node_or_err("}") end:pos()
-      { ast::Block{ span: FileSpan{start, end}, open, lets, actions, close } }
+        close:tok_node_or_err("}")
+      { ast::Block{ span, open, lets, actions, close } }
 
       rule action() -> ast::Action
-        = start:pos() KW("repeat") _ dir_count:(!['{'] dir:expr() _ count:expr() { (dir, count) })? _ block:block() end:pos()
-          { ast::Action::Repeat(ast::ActionRepeat{ span: FileSpan{start, end}, dir_count, block }) }
-        / start:pos() KW("on") _ name:IDENTIFIER() _ args:expr_tup()? _ block:block()? end:pos()
-          { ast::Action::On(ast::ActionOn{ span: FileSpan{start, end}, name, args, block }) }
-        / start:pos() KW("for") _ vars:COMMASEP_ONE(<(l:IDENTIFIER() _ "=" _ r:expr() { (l,r) })>) _ block:block() end:pos()
-          { ast::Action::For(ast::ActionFor{ span: FileSpan{start, end}, vars, block }) }
-        / start:pos() KW("any") _ open:tok_node("{") _ arms:action()**__ _ close:tok_node_or_err("}") end:pos()
-          { ast::Action::Any(ast::ActionAny { span: FileSpan{start, end}, arms }) }
-        / start:pos() KW("alt") _ dir:expr() _ expr:expr() _ open:tok_node("{") _ arms:alt_arm()**__ _ close:tok_node_or_err("}") end:pos()
-          { ast::Action::Alt(ast::ActionAlt{ span: FileSpan{start, end}, dir, expr, arms }) }
-        / start:pos() process:process() end:pos()
+        = KW("repeat") _ dir_count:(!['{'] dir:expr() _ count:expr() { (dir, count) })? _ block:block()
+          { ast::Action::Repeat(ast::ActionRepeat{ span, dir_count, block }) }
+        / KW("on") _ name:IDENTIFIER() _ args:expr_tup()? _ block:block()?
+          { ast::Action::On(ast::ActionOn{ span, name, args, block }) }
+        / KW("for") _ vars:COMMASEP_ONE(<(l:IDENTIFIER() _ "=" _ r:expr() { (l,r) })>) _ block:block()
+          { ast::Action::For(ast::ActionFor{ span, vars, block }) }
+        / KW("any") _ open:tok_node("{") _ arms:action()**__ _ close:tok_node_or_err("}")
+          { ast::Action::Any(ast::ActionAny { span, arms }) }
+        / KW("alt") _ dir:expr() _ expr:expr() _ open:tok_node("{") _ arms:alt_arm()**__ _ close:tok_node_or_err("}")
+          { ast::Action::Alt(ast::ActionAlt { span, dir, expr, arms }) }
+        / process:process()
           { ast::Action::Process(process) }
         / e:skip_recover("action", <action_recovery_set()>) { e.into() }
 
           rule alt_arm() -> ast::AltArm
-            = start:pos() discriminant:expr() _ "=>" _ block:block() end:pos()
-            { ast::AltArm { span: FileSpan{start, end}, discriminant, block } }
+            = discriminant:expr() _ "=>" _ block:block()
+            { ast::AltArm { span, discriminant, block } }
 
   // Expressions
   rule expr_tup() -> ast::ExprTup
-      = start:pos() open:tok_node("(") fields:COMMASEP(<(![')'] e:tuple_field() {e})>) close:tok_node_or_err(")") end:pos()
-        { ast::ExprTup { span: FileSpan{start, end}, open, fields, close }}
+      = open:tok_node("(") fields:COMMASEP(<(![')'] e:tuple_field() {e})>) close:tok_node_or_err(")")
+        { ast::ExprTup { span, open, fields, close }}
 
     rule tuple_field() -> ast::TupleField
-      = start:pos() name:(name: IDENTIFIER() _ "=" _ {name})? expr:expr() end:pos()
-        { ast::TupleField { span: FileSpan{start, end}, name, expr } }
+      = name:(name: IDENTIFIER() _ "=" _ {name})? expr:expr()
+        { ast::TupleField { span, name, expr } }
 
   pub rule expr() -> ast::Expr = precedence! {
-    start:position!() e:@ end:position!() { let e:ast::Expr = e; e.with_span(FileSpan::from(start..end)) }
+    l:@ _ "=>" _ r:(@)  { ast::ExprFunc { span, args: Box::new(l), body: Box::new(r) }.into() }
+    RAW_KW("type") _ expr:@        { ast::ExprType { span, expr: Box::new(expr) }.into() }
     --
-    l:@ _ "=>" _ r:(@)  { ast::ExprFunc { span: FAKE_SPAN, args: Box::new(l), body: Box::new(r) }.into() }
-    RAW_KW("type") _ expr:@        { ast::ExprType { span: FAKE_SPAN, expr: Box::new(expr) }.into() }
+    l:@ _ "!" _ r:(@)   { ast::ExprFlip { span, dn: Some(Box::new(l)), up: Some(Box::new(r)) }.into() } // TODO: nonassociative
+    "~" _ x:@           { ast::ExprFlip { span, dn: Some(Box::new(x)), up: None }.into() }
     --
-    l:@ _ "!" _ r:(@)   { ast::ExprFlip { span: FAKE_SPAN, dn: Some(Box::new(l)), up: Some(Box::new(r)) }.into() } // TODO: nonassociative
-    "~" _ x:@           { ast::ExprFlip { span: FAKE_SPAN, dn: Some(Box::new(x)), up: None }.into() }
+    l:(@) _ ":" _ r:@ { ast::ExprTyped { span, expr:Box::new(l), ty: Box::new(r) }.into() }
     --
-    l:(@) _ ":" _ r:@ { ast::ExprTyped { span: FAKE_SPAN, expr:Box::new(l), ty: Box::new(r) }.into() }
+    l:@ _ "|" _ r:(@)   { ast::ExprUnion { span, items: vec![l, r] }.into() }
     --
-    l:@ _ "|" _ r:(@)   { ast::ExprUnion { span: FAKE_SPAN, items: vec![l, r] }.into() }
+    l:@ _ ".." r:(_ &[^')'] r:expr() {r})? step:(__ "by" _ s:expr() {s})?  { ast::ExprRange { span, lo: Box::new(l), hi: r.map(Box::new), step: step.map(Box::new) }.into() }
     --
-    l:@ _ ".." r:(_ &[^')'] r:expr() {r})? step:(__ "by" _ s:expr() {s})?  { ast::ExprRange { span: FAKE_SPAN, lo: Box::new(l), hi: r.map(Box::new), step: step.map(Box::new) }.into() }
+    l:(@) _ "+" _ r:@   { ast::ExprBin { span, l: Box::new(l), op: BinOp::Add, r:Box::new(r) }.into() }
+    l:(@) _ "-" _ r:@   { ast::ExprBin { span, l: Box::new(l), op: BinOp::Sub, r:Box::new(r) }.into() }
     --
-    l:(@) _ "+" _ r:@   { ast::ExprBin { span: FAKE_SPAN, l: Box::new(l), op: BinOp::Add, r:Box::new(r) }.into() }
-    l:(@) _ "-" _ r:@   { ast::ExprBin { span: FAKE_SPAN, l: Box::new(l), op: BinOp::Sub, r:Box::new(r) }.into() }
+    l:(@) _ "*" _ r:@   { ast::ExprBin { span, l: Box::new(l), op: BinOp::Mul, r: Box::new(r) }.into() }
+    l:(@) _ "/" _ r:@   { ast::ExprBin { span, l: Box::new(l), op: BinOp::Div, r: Box::new(r) }.into() }
     --
-    l:(@) _ "*" _ r:@   { ast::ExprBin { span: FAKE_SPAN, l: Box::new(l), op: BinOp::Mul, r: Box::new(r) }.into() }
-    l:(@) _ "/" _ r:@   { ast::ExprBin { span: FAKE_SPAN, l: Box::new(l), op: BinOp::Div, r: Box::new(r) }.into() }
+    e:@ "[" choices:COMMASEP(<k:expr() _ "=" _ v:expr() { (k,v) }>) "]" { ast::ExprChoose { span, e: Box::new(e), choices }.into() }
+    e:@ arg:expr_tup() { ast::ExprCall { span, func: Box::new(e), arg }.into() }
     --
-    e:@ "[" choices:COMMASEP(<k:expr() _ "=" _ v:expr() { (k,v) }>) "]" { ast::ExprChoose { span: FAKE_SPAN, e: Box::new(e), choices }.into() }
-    e:@ arg:expr_tup() { ast::ExprCall { span: FAKE_SPAN, func: Box::new(e), arg }.into() }
-    --
-    start:pos() value:literal() end:pos() { ast::ExprLiteral { span: FileSpan { start, end }, value }.into() }
-    start:pos() value:STRING() end:pos() { ast::ExprString { span: FileSpan { start, end }, value }.into() }
-    start:pos() "_" end:pos() { ast::ExprIgnore { span: FileSpan { start, end } }.into() }
-    start:pos() "[" _ elem:expr() _ ";" _ count:expr() _ "]" end:pos() { ast::ExprArrayRep { span: FileSpan { start, end }, elem: Box::new(elem), count: Box::new(count) }.into() }
-    start:pos() "[" _ elems:COMMASEP(<concat_elem()>) _ "]" end:pos() { ast::ExprConcat { span: FileSpan { start, end }, elems }.into() }
+    value:literal() { ast::ExprLiteral { span, value }.into() }
+    value:STRING() { ast::ExprString { span, value }.into() }
+    "_" { ast::ExprIgnore { span }.into() }
+    "[" _ elem:expr() _ ";" _ count:expr() _ "]" { ast::ExprArrayRep { span, elem: Box::new(elem), count: Box::new(count) }.into() }
+    "[" _ elems:COMMASEP(<concat_elem()>) _ "]" { ast::ExprConcat { span, elems }.into() }
     i:IDENTIFIER() { i.into() }
     e:expr_tup() { e.into() }
   } / e:recover("expression", <action_recovery_set() / [',' | ')' | ']' | '}'] {}>) { e.into() }
@@ -178,21 +173,21 @@ peg::parser!(pub grammar signalspec() for str {
 
   #[cache_left_rec]
   pub rule process() -> ast::Process
-      = start:pos() lower:process() _ "|" _ upper:process() end:pos() {
-          ast::Process::Stack(ast::ProcessStack{ span: FileSpan { start, end }, lower: Box::new(lower), upper: Box::new(upper) })
+      = lower:process() _ "|" _ upper:process() {
+          ast::Process::Stack(ast::ProcessStack{ span, lower: Box::new(lower), upper: Box::new(upper) })
       }
       / RAW_KW("seq") _ block:block() { ast::Process::Seq(block) }
-      / start:pos() "new" _ top:protocol_ref() _ block:block() end:pos() {
-          ast::Process::New(ast::ProcessNew { span: FileSpan { start, end }, top, block })
+      / "new" _ top:protocol_ref() _ block:block() {
+          ast::Process::New(ast::ProcessNew { span, top, block })
       }
-      / start:pos() "primitive" _ name:IDENTIFIER() _ args:expr_tup() end:pos() {
-          ast::Process::Primitive(ast::ProcessPrimitive { span: FileSpan { start, end }, name, args })
+      / "primitive" _ name:IDENTIFIER() _ args:expr_tup() {
+          ast::Process::Primitive(ast::ProcessPrimitive { span, name, args })
       }
-      / start:pos() name:IDENTIFIER() _ args:expr_tup() end:pos() {
-          ast::Process::Call(ast::ProcessCall { span: FileSpan { start, end }, name, args })
+      / name:IDENTIFIER() _ args:expr_tup() {
+          ast::Process::Call(ast::ProcessCall { span, name, args })
       }
-      / start:pos() name:IDENTIFIER() end:pos() {
-          ast::Process::Child(ast::ProcessChild { span: FileSpan { start, end }, name })
+      / name:IDENTIFIER() {
+          ast::Process::Child(ast::ProcessChild { span, name })
       }
 
   // Lexer
